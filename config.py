15832253e2b19f561acd9946216fc32d6a641468
aferion7 <phantomgaken@gmail.com>
2026-06-14 17:23:20 +0500

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
+API_ID = None
+API_HASH = None
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 OWNER_ID = int(os.getenv("OWNER_ID"))
-SESSION_STRING = os.getenv("SESSION_STRING")
+SESSION_STRING = None
