a4d14d7c83004db60cce4fc456fdb86a24d3e8af
aferion7 <phantomgaken@gmail.com>
2026-06-14 17:00:16 +0500

    Update config.py

--- config.py
+++ config.py
@@ -7,4 +7,4 @@ API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 OWNER_ID = int(os.getenv("OWNER_ID"))
-SESSION_NAME = "spy_session"
+SESSION_STRING = os.getenv("SESSION_STRING")
